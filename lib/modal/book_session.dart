949682f8fa1182fadf6315006b790951b5352585
HP <developer.amrit.randhawa@gmail.com>
2023-03-07 21:09:35 +0530

    update

--- lib/modal/book_session.dart
+++ lib/modal/book_session.dart
@@ -8,7 +8,11 @@ class BookSession extends StatefulWidget {
   final String clientName;
   final int pendingMassage;
 
-  const BookSession({super.key, required this.number, required this.clientName, required this.pendingMassage});
+  const BookSession(
+      {super.key,
+      required this.number,
+      required this.clientName,
+      required this.pendingMassage});
 
   @override
   State<BookSession> createState() => _BookSessionState();
@@ -18,9 +22,18 @@ class _BookSessionState extends State<BookSession> {
   String manager = FirebaseAuth.instance.currentUser!.email.toString();
   final db = FirebaseFirestore.instance;
   String dropdownValue = list.first;
-  static const List<String> list = <String>['Deep Tissue', 'Foot Massage', 'Aroma Massage', 'Full Body Massage'];
+ late List<dynamic> massagesName;
+  static const List<String> list = <String>[
+    'Deep Tissue',
+    'Foot Massage',
+    'Aroma Massage',
+    'Full Body Massage'
+  ];
+
+
   @override
   void initState() {
+    check();
     super.initState();
   }
 
@@ -36,64 +49,68 @@ class _BookSessionState extends State<BookSession> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Column(
-
             children: [
               const SizedBox(height: 20),
-                   CircleAvatar( maxRadius: MediaQuery.of(context).size.width/7, child: Image.asset("assets/business.png")),
+              CircleAvatar(
+                  maxRadius: MediaQuery.of(context).size.width / 7,
+                  child: Image.asset("assets/business.png")),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children:  [
+                children: [
                   const SizedBox(height: 10),
-                  Text(widget.clientName,style: const TextStyle(fontSize: 22,fontFamily: "Dosis"),)
+                  Text(
+                    widget.clientName,
+                    style: const TextStyle(fontSize: 22, fontFamily: "Dosis"),
+                  )
                 ],
               ),
               const SizedBox(height: 10),
               const Text("Your booking manager"),
-              Center(child: Text(manager,style: const TextStyle(fontSize: 20,fontWeight: FontWeight.w400),)),
+              Center(
+                  child: Text(
+                manager,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              )),
               const SizedBox(height: 15),
-
-
-        Card(
-elevation: 20,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width-30,
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text("Choose a massage service",style: TextStyle(fontFamily: "Montserrat",fontSize: 20)),
+              Card(
+                elevation: 20,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width - 30,
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("Choose a massage service",
+                            style: TextStyle(
+                                fontFamily: "Montserrat", fontSize: 20)),
+                      ),
+                      const SizedBox(height: 15),
+                      DropdownButton<String>(
+                        itemHeight: 50,
+                        onTap: () {},
+                        value: dropdownValue,
+                        icon: const Icon(Icons.add),
+                        elevation: 15,
+                        style: const TextStyle(color: Colors.deepPurple),
+                        onChanged: (String? value) {
+                          // This is called when the user selects an item.
+                          setState(() {
+                            dropdownValue = value!;
+                          });
+                        },
+                        items:
+                            list.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
                 ),
-                const SizedBox(height: 15),
-                DropdownButton<String>(
-                  itemHeight: 50,
-                  onTap: (){
-
-                  },
-                  value: dropdownValue,
-                  icon: const Icon(Icons.add),
-                  elevation: 15,
-                  style: const TextStyle(color: Colors.deepPurple),
-
-                  onChanged: (String? value) {
-                    // This is called when the user selects an item.
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                  },
-                  items: list.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>
-                      (
-
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
-          ),
-        )
-
+              ),
             ],
           ),
         ),
@@ -101,7 +118,8 @@ elevation: 20,
     );
   }
 
-  Future<void> createBooking(String clientNumber,String spaName, String massage, double duration) async {
+  Future<void> createBooking(String clientNumber, String spaName,
+      String massage, double duration) async {
     await FirebaseFirestore.instance.enableNetwork();
     Timestamp timestamp = Timestamp.now();
     FirebaseFirestore.instance.collection("clients").doc(clientNumber).update({
@@ -110,10 +128,27 @@ elevation: 20,
           "spaName": spaName,
           "massage": massage,
           "duration": duration,
-          "Date":   DateFormat.yMMMd().add_jm().format(timestamp.toDate()),
-          "bookingManager" : manager
+          "Date": DateFormat.yMMMd().add_jm().format(timestamp.toDate()),
+          "bookingManager": manager
         }
       ]),
+    });
+  }
+
+  void check() {
+    db
+        .collection("spaServices")
+        .doc("Heritage Spa")
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+         massagesName = documentSnapshot.get("allServices");
+         for(int i  = 0; i < massagesName.length; i++){
+           print(massagesName[i]["name"]);
+           print(massagesName[i]["rate"]);
+           print(massagesName[i]["subHeading"]);
+
+         }
+
     });
   }
 }
